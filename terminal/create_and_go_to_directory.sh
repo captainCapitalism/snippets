487600f13520cc9b73e8eb2793d14910c40e8220
michal bargiel <michal5bargiel@gmail.com>
2022-06-11 17:25:10 +0200

    ✨ Create and go to directory.

---
+++ terminal/create_and_go_to_directory.sh
@@ -0,0 +1,5 @@
+# This snippet creates directory DIRNAME and goes to it after creation.
+# I find it interesting because it doesn't require writing directory name twice.
+# Found at https://www.valentinog.com/blog/cookies/
+
+mkdir DIRNAME && cd $_
